12ecdc241d4d61b2bd71567f9469f16631a5728c
Ewan Wallace <ewan.wallace@roche.com>
2025-03-24 16:30:46 +0000

    added typing

--- src/paper_query/llm/chains.py
+++ src/paper_query/llm/chains.py
@@ -1,11 +1,12 @@
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts.chat import ChatPromptTemplate
 from langchain_core.runnables.base import RunnableSequence
 
 from .models import get_model
 
 
 def get_chain(
-    model_name: str, model_provider: str, prompt, additional_keys: dict = {}
+    model_name: str, model_provider: str, prompt: ChatPromptTemplate, additional_keys: dict = {}
 ) -> RunnableSequence:
     """Get the chain for the chatbot."""
     model = get_model(model_name, model_provider)
