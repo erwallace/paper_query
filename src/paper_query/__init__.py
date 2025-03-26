2cf2610540043d54a98dc681050f7b44eccdd90c
Ewan Wallace <ewan.wallace@roche.com>
2025-03-26 14:26:53 +0000

    code-query cmdline function

--- src/paper_query/__init__.py
+++ src/paper_query/__init__.py
@@ -1,23 +1,25 @@
+import dataclasses
 from pathlib import Path
 
 from .base_chatbot import main as base_chatbot
+from .code_query_chatbot import main as code_query_chatbot
 from .paper_query_chatbot import main as paper_query_chatbot
 from .paper_query_plus_chatbot import main as paper_query_plus_chatbot
 
-# Directories
-project_dir: Path = Path(__file__).resolve().parents[2]
-src_dir: Path = project_dir / "src"
-test_dir: Path = project_dir / "tests"
-data_dir: Path = project_dir / "data"
-assets_dir: Path = project_dir / "assets"
+
+@dataclasses.dataclass
+class paths:
+    project_dir: Path = Path(__file__).resolve().parents[2]
+    src_dir: Path = project_dir / "src"
+    test_dir: Path = project_dir / "tests"
+    data_dir: Path = project_dir / "data"
+    assets_dir: Path = project_dir / "assets"
+
 
 __all__ = [
     "base_chatbot",
     "paper_query_chatbot",
     "paper_query_plus_chatbot",
-    "project_dir",
-    "src_dir",
-    "test_dir",
-    "data_dir",
-    "assets_dir",
+    "code_query_chatbot",
+    "paths",
 ]
---
+++ src/paper_query/code_query_chatbot.py
@@ -0,0 +1,41 @@
+"""
+paper-query-v1 [model] [provider] [paper]
+
+A chatbot for querying a single paper. The entire paper is held in context.
+"""
+
+import argparse
+
+from paper_query.chatbots import CodeQueryChatbot
+from paper_query.ui import cli_chatbot
+
+
+def main():
+    parser = argparse.ArgumentParser(description="LangChain CLI Chatbot")
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="gpt-4o",
+        help="Model name to use for the chatbot",
+    )
+    parser.add_argument(
+        "--provider",
+        type=str,
+        default="openai",
+        help="Model provider to use for the chatbot",
+    )
+    parser.add_argument(
+        "--paper",
+        type=str,
+        default="./assets/strainrelief_preprint.pdf",
+        help="Path to the paper for the chatbot",
+    )
+    args = parser.parse_args()
+
+    # Initialize chatbot with paper and run chatbot
+    chatbot = CodeQueryChatbot(args.model, args.provider, args.paper)
+    cli_chatbot(chatbot)
+
+
+if __name__ == "__main__":
+    main()
