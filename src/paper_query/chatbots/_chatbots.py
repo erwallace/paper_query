24b7da0da78c69a21a3352dafaf9b1ee0302b792
Ewan Wallace <ewan.wallace@roche.com>
2025-03-26 15:16:10 +0000

    use RAG_DOC_ID to identify RAG vectors

--- src/paper_query/chatbots/_chatbots.py
+++ src/paper_query/chatbots/_chatbots.py
@@ -1,15 +1,16 @@
-import os
 from collections.abc import Generator
 
 from langchain.memory import ConversationBufferMemory
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
 
+from paper_query.constants import RAG_DOC_ID
 from paper_query.data.loaders import code_loader, pypdf_loader, references_loader
 from paper_query.data.processors import split_documents
 from paper_query.llm import get_chain
 from paper_query.llm.prompts import (
     base_prompt,
     code_query_prompt,
+    hybrid_query_prompt,
     paper_query_plus_prompt,
     paper_query_prompt,
 )
@@ -109,7 +110,7 @@ class PaperQueryPlusChatbot(BaseChatbot):
         # Retrieve relevant reference information
         relevant_docs = self.retriever.invoke(user_input)
         relevant_references = "\n".join(
-            [f"From {doc.metadata['filename']}:\n{doc.page_content}" for doc in relevant_docs]
+            [f"From {doc.metadata[RAG_DOC_ID]}:\n{doc.page_content}" for doc in relevant_docs]
         )
 
         return super().stream_response(
@@ -162,7 +163,7 @@ class CodeQueryChatbot(BaseChatbot):
         # Retrieve relevant reference information
         relevant_docs = self.retriever.invoke(user_input)
         relevant_code = "\n".join(
-            [f"From {doc.metadata['file_path']}:\n{doc.page_content}" for doc in relevant_docs]
+            [f"From {doc.metadata[RAG_DOC_ID]}:\n{doc.page_content}" for doc in relevant_docs]
         )
 
         return super().stream_response(
@@ -179,33 +180,59 @@ class HybridQueryChatbot(BaseChatbot):
         model_provider: str,
         paper_path: str,
         references_dir: str,
-        code_dir: str,
+        github_repo_url: str = "https://github.com/prescient-design/StrainRelief.git",
+        split_method: str = "recursive",
+        embedding_method: str = "openai",
+        retriever_method: str = "base",
+        **kwargs,
     ):
         super().__init__(model_name, model_provider)
+
+        # Load the main paper
+        self.paper_text = pypdf_loader(paper_path)
+
+        # Load code
+        self.code = code_loader(github_repo_url)
+
+        # Process references for embeddings
+        self.references = references_loader(references_dir)
+        split_docs = split_documents(
+            self.references, method=split_method, **kwargs.get("split_kwargs", {})
+        )
+
+        # Create vectorstore and retriever
+        self.vectorstore = create_vectorstore(
+            self.code + split_docs,
+            embedding_method=embedding_method,
+            **kwargs.get("embedding_kwargs", {}),
+        )
+        self.retriever = setup_retriever(
+            self.vectorstore, method=retriever_method, **kwargs.get("retriever_kwargs", {})
+        )
+
+        # Update the chain
         self.chain = get_chain(
             model_name,
             model_provider,
-            prompt=paper_query_plus_prompt,
+            prompt=hybrid_query_prompt,
             additional_keys={
                 "paper_text": lambda x: x["paper_text"],
-                "references": lambda x: x["references"],
-                "code_dir": lambda x: x["code_dir"],
+                "relevant_references": lambda x: x["relevant_references"],
+                "relevant_code": lambda x: x["relevant_code"],
             },
         )
-        self.paper_text = pypdf_loader(paper_path)
-
-        self.references = []
-        for file in os.listdir(references_dir):
-            self.references.append(pypdf_loader(os.path.join(references_dir, file)))
-
-        self.code_dir = ""
 
     def stream_response(self, user_input: str) -> Generator[str, None, None]:
+        # Retrieve relevant reference information
+        relevant_docs = self.retriever.invoke(user_input)
+        relevant_references = "\n".join(
+            [f"From {doc.metadata[RAG_DOC_ID]}:\n{doc.page_content}" for doc in relevant_docs]
+        )
+
         return super().stream_response(
             user_input,
             {
                 "paper_text": self.paper_text,
-                "references": self.references,
-                "code_dir": self.code_dir,
+                "relevant_references": relevant_references,
             },
         )
