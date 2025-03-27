8c7af81c8eaf065cfcbe5f49f8e2e787e236decf
Ewan Wallace <ewan.wallace@roche.com>
2025-03-27 22:33:16 +0000

    added kwargs

--- src/paper_query/llm/models.py
+++ src/paper_query/llm/models.py
@@ -3,7 +3,7 @@ import os
 from langchain.chat_models import init_chat_model
 
 
-def get_model(model_name: str, model_provider: str):
+def get_model(model_name: str, model_provider: str, **kwargs):
     """Initialize the chat model."""
     if model_provider == "openai":
         from paper_query.constants import OPENAI_API_KEY
@@ -14,4 +14,4 @@ def get_model(model_name: str, model_provider: str):
 
         os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 
-    return init_chat_model(model_name, model_provider=model_provider)
+    return init_chat_model(model_name, model_provider=model_provider, **kwargs)
