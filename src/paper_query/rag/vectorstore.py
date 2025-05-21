431720c16f694f8c086c75c2cfb50e73394a1f39
Ewan Wallace <ewan.wallace@roche.com>
2025-05-21 20:09:16 +0100

    moved EMBEDDING_METHODS to global dict

--- src/paper_query/rag/vectorstore.py
+++ src/paper_query/rag/vectorstore.py
@@ -1,9 +1,12 @@
 import os
+from pathlib import Path
 
 from langchain.schema import Document
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import OpenAIEmbeddings
+
+PERSIST_DIRECTORY = str(Path(__file__).resolve().parents[3] / "vectorstore")
 
 
 def openai_embeddings():
@@ -20,17 +23,41 @@ def huggingface_embeddings(model_name="sentence-transformers/all-mpnet-base-v2")
     return HuggingFaceEmbeddings(model_name=model_name)
 
 
+EMBEDDING_METHODS = {
+    "openai": openai_embeddings,
+    "huggingface": huggingface_embeddings,
+}
+
+
+def get_embedding_method(embedding_method: str) -> callable:
+    """Retrieve the embedding method based on the provided name."""
+    if embedding_method not in EMBEDDING_METHODS:
+        raise KeyError(f"Unsupported embedding method: {embedding_method}")
+
+    return EMBEDDING_METHODS[embedding_method]
+
+
 def create_vectorstore(
     documents: list[Document], embedding_method: str = "openai", **kwargs
 ) -> Chroma:
     """Create a vectorstore from a list of documents."""
-    embedding_methods = {
-        "openai": openai_embeddings,
-        "huggingface": huggingface_embeddings,
-    }
-
-    if embedding_method not in embedding_methods:
-        raise ValueError(f"Unsupported embedding method: {embedding_method}")
-
-    embeddings = embedding_methods[embedding_method](**kwargs)
+    embeddings = get_embedding_method(embedding_method)(**kwargs)
     return Chroma.from_documents(documents, embeddings)
+
+
+def setup_vectorstore(
+    persist_directory: str = PERSIST_DIRECTORY,
+    embedding_method: str = "openai",
+    **embedding_kwargs,
+) -> Chroma:
+    """Set up a vectorstore."""
+    if not os.path.exists(persist_directory):
+        raise ValueError(f"Persist directory does not exist: {persist_directory}")
+
+    embeddings = get_embedding_method(embedding_method)(**embedding_kwargs)
+    return Chroma(
+        persist_directory=persist_directory,
+        embedding_function=embeddings,
+        create_collection_if_not_exists=False,
+        collection_name="paper-query",
+    )
