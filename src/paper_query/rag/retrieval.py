eaa3d0cf87bc163cc84c6480000fa043e595e3aa
Ewan Wallace <ewan.wallace@roche.com>
2025-05-21 20:08:53 +0100

    moved RETRIEVER_METHODS to global dict

--- src/paper_query/rag/retrieval.py
+++ src/paper_query/rag/retrieval.py
@@ -17,14 +17,15 @@ def contextual_compression_retriever(vectorstore: Chroma, llm, k: int = 5):
     # )
 
 
+RETREIVER_METHODS = {
+    "base": base_retriever,
+    # "contextual_compression": contextual_compression_retriever,
+}
+
+
 def setup_retriever(vectorstore: Chroma, method: str = "base", **kwargs):
     """Set up a retriever."""
-    retriever_methods = {
-        "base": base_retriever,
-        "contextual_compression": contextual_compression_retriever,
-    }
+    if method not in RETREIVER_METHODS:
+        raise KeyError(f"Unsupported retriever method: {method}")
 
-    if method not in retriever_methods:
-        raise ValueError(f"Unsupported retriever method: {method}")
-
-    return retriever_methods[method](vectorstore, **kwargs)
+    return RETREIVER_METHODS[method](vectorstore, **kwargs)
