fd2d20b9ed09daaa234e9034769d41aa5b3c7075
Ewan Wallace <ewan.wallace@roche.com>
2025-05-22 11:46:32 +0100

    import persist_directory

--- src/paper_query/rag/vectorstore.py
+++ src/paper_query/rag/vectorstore.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 
 from langchain.schema import Document
 from langchain_community.vectorstores import Chroma
@@ -7,7 +6,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import OpenAIEmbeddings
 from loguru import logger
 
-PERSIST_DIRECTORY = str(Path(__file__).resolve().parents[3] / "vectorstore")
+from paper_query.constants import PERSIST_DIRECTORY
 
 
 def openai_embeddings():
