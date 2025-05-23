a3b53f04257fdff4c57183e7844d8d0a3274f475
Ewan Wallace <ewan.wallace@roche.com>
2025-05-23 08:32:22 +0100

    added default chatbots

--- src/paper_query/constants/__init__.py
+++ src/paper_query/constants/__init__.py
@@ -1,6 +1,6 @@
 from ._api_keys import GROQ_API_KEY, HUGGINGFACE_API_KEY, OPENAI_API_KEY
 from ._paths import PERSIST_DIRECTORY, assets_dir, data_dir, project_dir, src_dir, test_dir
-from ._strings import RAG_DOC_ID
+from ._strings import RAG_DOC_ID, STREAMLIT_CHEAP_MODEL, STREAMLIT_EXPENSIVE_MODEL
 
 __all__ = [
     "OPENAI_API_KEY",
@@ -13,4 +13,6 @@ __all__ = [
     "data_dir",
     "assets_dir",
     "RAG_DOC_ID",
+    "STREAMLIT_CHEAP_MODEL",
+    "STREAMLIT_EXPENSIVE_MODEL",
 ]
